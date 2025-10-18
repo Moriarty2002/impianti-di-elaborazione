b43c1aa5707e9c7ab550012b50fd5d5060bb2862
Kratos1702 <mespositow@gmail.com>
2025-10-18 12:53:57 +0200

    add script to generate files

---
+++ homework/generate_file_scrypt.sh
@@ -0,0 +1,27 @@
+    sudo truncate -s 1K light_1.ico
+    sudo truncate -s 5K light_2.css
+    sudo truncate -s 9K light_3.js
+    sudo truncate -s 14K light_4.svg
+    sudo truncate -s 18K light_5.png
+    sudo truncate -s 22K light_6.txt
+    sudo truncate -s 35K light_7.xml
+    sudo truncate -s 50K light_8.jpg
+    sudo truncate -s 75K light_9.woff2
+    sudo truncate -s 90K light_10.js
+
+    sudo truncate -s 120K medium_1.jpg
+    sudo truncate -s 180K medium_2.js
+    sudo truncate -s 250K medium_3.pdf
+    sudo truncate -s 400K medium_4.gif
+    sudo truncate -s 650K medium_5.jpg
+    sudo truncate -s 800K medium_6.css
+    sudo truncate -s 950K medium_7.html
+    sudo truncate -s 1200K medium_8.mp3
+    sudo truncate -s 1500K medium_9.js
+    sudo truncate -s 1800K medium_10.json
+
+    sudo truncate -s 2500K heavy_1.mp4
+    sudo truncate -s 3800K heavy_2.zip
+    sudo truncate -s 5000K heavy_3.csv
+    sudo truncate -s 7200K heavy_4.pdf
+    sudo truncate -s 9500K heavy_5.jpg
